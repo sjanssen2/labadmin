4ae7b9e4ec34b82764e7c06a793ce55762965714
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-09-30 11:20:30 -0700

    redesign of adding objects to three col tables

--- knimin/lib/data_access_pm.py
+++ knimin/lib/data_access_pm.py
@@ -3,6 +3,92 @@ from knimin.lib.exceptions import *
 from sql_connection import TRN
 
 
+def _add_object(tbl_name, name, notes=None):
+    """ Generalized function to add a new object in a table with three columns:
+        (object_id, name, notes), e.g. processing_robot, tm300_8_tool, ...
+
+    Parameters
+    ----------
+    tbl_name: str
+        The DB table name into which the new object should be added.
+    name: str
+        Name of the new processing-robot.
+    notes: str
+        Some notes about new new processing-robot.
+
+    Raises
+    ------
+    LabadminDBError
+        a) If the tbl_name does not exist in the DB.
+        b) If the table in which the object should be added does not follow the
+           expected design, which is that the table must have exactly the three
+           columns: [tbl_name_id, name, notes]
+    ValueError
+        If the object name is missing or empty.
+    LabadminDBDuplicateError
+        If an object of the same name already exists.
+    """
+
+    with TRN:
+        # check that tbl_name exists in DB
+        sql = """SELECT DISTINCT table_name from INFORMATION_SCHEMA.COLUMNS
+                 WHERE table_schema = 'pm' AND table_name = %s"""
+        TRN.add(sql, [tbl_name])
+        if len(TRN.execute_fetchindex()) != 1:
+            raise LabadminDBError('Table %s does not exist in data base.' %
+                                  tbl_name)
+
+        # check that table has exactly three columns
+        sql = """SELECT column_name from INFORMATION_SCHEMA.COLUMNS
+                 WHERE table_schema = 'pm' AND table_name = %s"""
+        TRN.add(sql, [tbl_name])
+        column_names = [c[0] for c in TRN.execute_fetchindex()]
+        if [tbl_name+'_id', 'name', 'notes'] != column_names:
+            raise LabadminDBError(('Table %s does not have the expected column'
+                                   ' design. It must have exactly the three '
+                                   'columns: "%s_id", "name", "notes"') %
+                                  (tbl_name, tbl_name))
+
+    # ensure that the object name is not empty
+    if not name or len(name) <= 0:
+        raise ValueError('The %s name cannot be empty.' % tbl_name)
+
+    with TRN:
+        # check if an object with the same name already exists
+        sql = """SELECT name FROM pm."""+tbl_name+""" WHERE name = %s"""
+        TRN.add(sql, [name])
+        if len(TRN.execute_fetchindex()) > 0:
+            raise LabadminDBDuplicateError(tbl_name,
+                                           'name: %s' % name)
+
+        # add the new object into the DB
+        sql = """INSERT INTO pm."""+tbl_name+""" (name, notes)
+                 VALUES (%s, %s)
+                 RETURNING %s_id"""
+        TRN.add(sql, [name, notes, tbl_name])
+        TRN.execute()
+
+
+def add_master_mix_lot(name, notes=None):
+    """ Adds a new master mix lot.
+
+    Parameters
+    ----------
+    name: str
+        Name of the new master mix lot.
+    notes: str
+        Some notes about new master mix lot.
+
+    Raises
+    ------
+    ValueError
+        If the master mix lot name is missing or empty.
+    LabadminDBDuplicateError
+        If a master mix lot of the same name already exists.
+    """
+    _add_object('master_mix_lot', name, notes)
+
+
 def add_processing_robot(name, notes=None):
     """ Adds a new processing-robot.
 
@@ -11,7 +97,7 @@ def add_processing_robot(name, notes=None):
     name: str
         Name of the new processing-robot.
     notes: str
-        Some notes about new new processing-robot.
+        Some notes about new processing-robot.
 
     Raises
     ------
@@ -20,25 +106,7 @@ def add_processing_robot(name, notes=None):
     LabadminDBDuplicateError
         If a processing robot of the same name already exists.
     """
-
-    # ensure that the processing robot name is not empty
-    if not name or len(name) <= 0:
-        raise ValueError('The processing robot name cannot be empty.')
-
-    with TRN:
-        # check if a processing robot with the same name already exists
-        sql = """SELECT name FROM pm.processing_robot WHERE name = %s"""
-        TRN.add(sql, [name])
-        if len(TRN.execute_fetchindex()) > 0:
-            raise LabadminDBDuplicateError('processing_robot',
-                                           'name: %s' % name)
-
-        # add the new processing robot to the DB
-        sql = """INSERT INTO pm.processing_robot (name, notes)
-                 VALUES (%s, %s)
-                 RETURNING processing_robot_id"""
-        TRN.add(sql, [name, notes])
-        TRN.execute()
+    _add_object('processing_robot', name, notes)
 
 
 def add_tm300_8_tool(name, notes=None):
@@ -49,7 +117,7 @@ def add_tm300_8_tool(name, notes=None):
     name: str
         Name of the new TM 300-8 tool.
     notes: str
-        Some notes about new new TM 300-8 tool.
+        Some notes about new TM 300-8 tool.
 
     Raises
     ------
@@ -58,25 +126,7 @@ def add_tm300_8_tool(name, notes=None):
     LabadminDBDuplicateError
         If a TM 300-8 tool of the same name already exists.
     """
-
-    # ensure that the TM 300-8 tool name is not empty
-    if not name or len(name) <= 0:
-        raise ValueError('The TM 300-8 tool name cannot be empty.')
-
-    with TRN:
-        # check if a TM 300-8 tool with the same name already exists
-        sql = """SELECT name FROM pm.tm300_8_tool WHERE name = %s"""
-        TRN.add(sql, [name])
-        if len(TRN.execute_fetchindex()) > 0:
-            raise LabadminDBDuplicateError('tm300_8_tool',
-                                           'name: %s' % name)
-
-        # add the new TM 300-8 tool to the DB
-        sql = """INSERT INTO pm.tm300_8_tool (name, notes)
-                 VALUES (%s, %s)
-                 RETURNING tm300_8_tool_id"""
-        TRN.add(sql, [name, notes])
-        TRN.execute()
+    _add_object('tm300_8_tool', name, notes)
 
 
 def add_tm50_8_tool(name, notes=None):
@@ -87,7 +137,7 @@ def add_tm50_8_tool(name, notes=None):
     name: str
         Name of the new TM 50-8 tool.
     notes: str
-        Some notes about new new TM 50-8 tool.
+        Some notes about new TM 50-8 tool.
 
     Raises
     ------
@@ -96,22 +146,24 @@ def add_tm50_8_tool(name, notes=None):
     LabadminDBDuplicateError
         If a TM 50-8 tool of the same name already exists.
     """
+    _add_object('tm50_8_tool', name, notes)
 
-    # ensure that the TM 50-8 tool name is not empty
-    if not name or len(name) <= 0:
-        raise ValueError('The TM 50-8 tool name cannot be empty.')
 
-    with TRN:
-        # check if a TM 50-8 tool with the same name already exists
-        sql = """SELECT name FROM pm.tm50_8_tool WHERE name = %s"""
-        TRN.add(sql, [name])
-        if len(TRN.execute_fetchindex()) > 0:
-            raise LabadminDBDuplicateError('tm50_8_tool',
-                                           'name: %s' % name)
+def add_water_lot(name, notes=None):
+    """ Adds a new water lot.
 
-        # add the new TM 50-8 tool to the DB
-        sql = """INSERT INTO pm.tm50_8_tool (name, notes)
-                 VALUES (%s, %s)
-                 RETURNING tm50_8_tool_id"""
-        TRN.add(sql, [name, notes])
-        TRN.execute()
+    Parameters
+    ----------
+    name: str
+        Name of the new water lot.
+    notes: str
+        Some notes about new water lot.
+
+    Raises
+    ------
+    ValueError
+        If the water lot name is missing or empty.
+    LabadminDBDuplicateError
+        If a water lot of the same name already exists.
+    """
+    _add_object('water_lot', name, notes)
--- knimin/lib/tests/test_data_access_pm.py
+++ knimin/lib/tests/test_data_access_pm.py
@@ -2,12 +2,20 @@ from unittest import TestCase, main
 
 from knimin import db
 from knimin.lib.data_access_pm import *
+from knimin.lib.data_access_pm import _add_object
 from knimin.lib.sql_connection import TRN
 from knimin.lib.exceptions import *
 
 
 class TestDataAccessPM(TestCase):
     def tearDown(self):
+        # _add_object
+        with TRN:
+            sql = """DELETE FROM pm.processing_robot
+                     WHERE name = %s"""
+            TRN.add(sql, ['robi2'])
+            TRN.execute()
+
         # add_processing_robot
         with TRN:
             sql = """DELETE FROM pm.processing_robot
@@ -29,19 +37,76 @@ class TestDataAccessPM(TestCase):
             TRN.add(sql, ['007ABC'])
             TRN.execute()
 
+        # test_add_water_lot
+        with TRN:
+            sql = """DELETE FROM pm.water_lot
+                     WHERE name = %s"""
+            TRN.add(sql, ['007ABC'])
+            TRN.execute()
+
     def setUp(self):
         db.add_processing_robot = add_processing_robot
         db.add_tm300_8_tool = add_tm300_8_tool
         db.add_tm50_8_tool = add_tm50_8_tool
+        db.add_water_lot = add_water_lot
+        db.add_master_mix_lot = add_master_mix_lot
+
+    def test__add_object(self):
+        # test check for table existence
+        self.assertRaisesRegexp(LabadminDBError,
+                                'does not exist in data base.',
+                                _add_object,
+                                'p1rocessing_robot',
+                                'robi2',
+                                'some notes')
+
+        # test check for table design
+        self.assertRaisesRegexp(LabadminDBError,
+                                'does not have the expected column',
+                                _add_object,
+                                'dna_plate',
+                                'robi2',
+                                'some notes')
+        # regular addition
+        _add_object('processing_robot', 'robi2', 'some notes')
+        # check that duplicates cannot be added
+        self.assertRaisesRegexp(LabadminDBDuplicateError,
+                                'already exists.',
+                                _add_object,
+                                'processing_robot',
+                                'robi2',
+                                'some notes')
 
     def test_add_processing_robot(self):
         self.assertRaisesRegexp(ValueError,
-                                'The processing robot name cannot be empty.',
+                                'The master_mix_lot name cannot be empty.',
+                                db.add_master_mix_lot,
+                                None,
+                                'some notes')
+        self.assertRaisesRegexp(ValueError,
+                                'The master_mix_lot name cannot be empty.',
+                                db.add_master_mix_lot,
+                                '',
+                                'some notes')
+        # add a master mix lot
+        name = 'robi1'
+        db.add_master_mix_lot(name, 'some notes')
+        # indirect test if the upper statement could add the robot, since it
+        # cannot be added twice with the same name.
+        self.assertRaisesRegexp(LabadminDBDuplicateError,
+                                'already exists.',
+                                db.add_master_mix_lot,
+                                name,
+                                'some notes')
+
+    def test_add_processing_robot(self):
+        self.assertRaisesRegexp(ValueError,
+                                'The processing_robot name cannot be empty.',
                                 db.add_processing_robot,
                                 None,
                                 'some notes')
         self.assertRaisesRegexp(ValueError,
-                                'The processing robot name cannot be empty.',
+                                'The processing_robot name cannot be empty.',
                                 db.add_processing_robot,
                                 '',
                                 'some notes')
@@ -58,12 +123,12 @@ class TestDataAccessPM(TestCase):
 
     def test_add_tm300_8_tool(self):
         self.assertRaisesRegexp(ValueError,
-                                'The TM 300-8 tool name cannot be empty.',
+                                'The tm300_8_tool name cannot be empty.',
                                 db.add_tm300_8_tool,
                                 None,
                                 'some notes')
         self.assertRaisesRegexp(ValueError,
-                                'The TM 300-8 tool name cannot be empty.',
+                                'The tm300_8_tool name cannot be empty.',
                                 db.add_tm300_8_tool,
                                 '',
                                 'some notes')
@@ -80,12 +145,12 @@ class TestDataAccessPM(TestCase):
 
     def test_add_tm50_8_tool(self):
         self.assertRaisesRegexp(ValueError,
-                                'The TM 50-8 tool name cannot be empty.',
+                                'The tm50_8_tool name cannot be empty.',
                                 db.add_tm50_8_tool,
                                 None,
                                 'some notes')
         self.assertRaisesRegexp(ValueError,
-                                'The TM 50-8 tool name cannot be empty.',
+                                'The tm50_8_tool name cannot be empty.',
                                 db.add_tm50_8_tool,
                                 '',
                                 'some notes')
@@ -97,6 +162,28 @@ class TestDataAccessPM(TestCase):
         self.assertRaisesRegexp(LabadminDBDuplicateError,
                                 'already exists.',
                                 db.add_tm50_8_tool,
+                                name,
+                                'some notes')
+
+    def test_add_water_lot(self):
+        self.assertRaisesRegexp(ValueError,
+                                'The water_lot name cannot be empty.',
+                                db.add_water_lot,
+                                None,
+                                'some notes')
+        self.assertRaisesRegexp(ValueError,
+                                'The water_lot name cannot be empty.',
+                                db.add_water_lot,
+                                '',
+                                'some notes')
+        # add a water_lot
+        name = '007ABC'
+        db.add_water_lot(name, 'some notes')
+        # indirect test if the upper statement could add the tool, since it
+        # cannot be added twice with the same name.
+        self.assertRaisesRegexp(LabadminDBDuplicateError,
+                                'already exists.',
+                                db.add_water_lot,
                                 name,
                                 'some notes')
 
